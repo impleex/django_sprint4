7cdf359d3a918e423203c86ff61d1cf9ce64f01e
impleex <koshkindany@yandex.ru>
2025-01-09 03:12:18 +0300

    pep

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -4,8 +4,25 @@ from django.views.generic import TemplateView
 app_name = 'pages'
 
 urlpatterns = [
-    path('about/', TemplateView.as_view(template_name='pages/about.html'), name='about'),
-    path('rules/', TemplateView.as_view(template_name='pages/rules.html'), name='rules'),
-    path('support/', TemplateView.as_view(template_name='pages/support.html'), name='support'),
-    path('faq/', TemplateView.as_view(template_name='pages/faq.html'), name='faq'),
+    path(
+        'faq/',
+        TemplateView.as_view(template_name='pages/faq.html'),
+        name='faq'
+    ),
+    path(
+        'support/',
+        TemplateView.as_view(template_name='pages/support.html'),
+        name='support'
+    ),
+    path(
+        'about/',
+        TemplateView.as_view(template_name='pages/about.html'),
+        name='about'
+    ),
+    path(
+        'rules/',
+        TemplateView.as_view(template_name='pages/rules.html'),
+        name='rules'
+    ),
 ]
+
